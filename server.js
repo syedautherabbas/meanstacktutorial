a50277e9af6d40d7825bc74bd6cb34fb6d33cfec
Syed Auther Abbas <syedautherabbas@gmail.com>
2017-08-18 03:28:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ app.put('/contactlist/:id', function (req, res) {
   );
 });
 
-app.listen(3000);
-console.log("Server running on port 3000");
+app.listen(80);
+console.log("Server running on port 80");
